c1102078cb5d7c6c06d51b36fb2f7a8e4d870f9d
Rulo <andres@rulocode.com>
2022-08-26 18:56:21 -0500

    feat(blog): header

--- pages/index.js
+++ pages/index.js
@@ -47,21 +47,13 @@ export default function Home({ posts }) {
               </g>
             </svg>
           </div>
-          <h1>Next.js blog powered by Notion API</h1>
+          <h1>Hi, I am Rulo</h1>
+          <p>Web developer</p>
           <p>
-            This is an example of a Next.js blog with data fetched with Notions
-            API. The data comes from{" "}
-            <a href={`https://www.notion.so/${databaseId}`}>this table</a>. Get
-            the source code on{" "}
-            <a href="https://github.com/samuelkraft/notion-blog-nextjs">
-              Github
-            </a>{" "}
-            or read{" "}
-            <a href="https://samuelkraft.com/blog/building-a-notion-blog-with-public-api">
-              my blogpost
-            </a>{" "}
-            on building your own.
+            Creating the next web generation. I want to make sure that the
+            internet is a safe, satisfying and secure place for everyone.
           </p>
+          <button type="button">Let's Talk</button>
         </header>
 
         <h2 className={styles.heading}>All Posts</h2>
