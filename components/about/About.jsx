c3710ee43b7b23ae7665d2bbd93fdc3fb65f4a41
Rulo <andres@rulocode.com>
2022-09-08 20:46:36 -0500

    feat(hero): adding about

--- components/about/About.jsx
+++ components/about/About.jsx
@@ -1,27 +1,51 @@
 import React from "react";
 
 import Image from "next/image";
+import ProfilePhoto from "../../assets/rulo2.png";
 
 import styles from "./about.module.scss";
 import BackgroundShape2 from "../../assets/shape_top.svg";
+import BackgroundShape_down from "../../assets/shape_top.svg";
 
 export const About = () => {
   return (
-    <section className={styles.about}>
-      
-      <div className={styles.bg3}>
-        <Image
-          alt="Mountains"
-          src={BackgroundShape2}
-          layout="fill"
-          objectFit="contain"
-        />
-       
-      </div>
-      {/* <h2 className={styles["about-title"]}>
-          About me
-        </h2> */}
-      
-    </section>
+    <div>
+      <section className={styles.about}>
+        <div className={styles.bg3}>
+          <Image
+            alt="Mountains"
+            src={BackgroundShape2}
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <h2 className={styles["about-title"]}>About me</h2>
+
+        <div className={styles["about-img"]}>
+          <Image
+            alt="Mountains"
+            src={ProfilePhoto}
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <div className="wrapper">
+          <p className={styles["about-description"]}>
+            My name is Andrés Santana but you can call me Rulo, I have a passion
+            for web design and enjoy creating websites that are both
+            aesthetically pleasing and easy to use.
+          </p>
+          <a href="" className={styles["about-description-readMore"]}>read more</a>
+        </div>
+        <div className={styles.bg2}>
+          <Image
+            alt="Mountains"
+            src={BackgroundShape2}
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+      </section>
+    </div>
   );
 };
--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -5,78 +5,76 @@ import Image from "next/image";
 
 import styles from "./Hero.module.scss";
 import BackgroundShape from "../../assets/Fondo.svg";
-import BackgroundShape2 from "../../assets/shape_top.svg";
 import ProfilePhoto from "../../assets/rulo.png";
 
 export const Hero = () => {
   return (
-    <section className='wrapper'>
-     <div className={styles.hero}>
-       <div className={styles.bg1}>
-         <Image
-           alt="Mountains"
-           src={BackgroundShape}
-           layout="fill"
-           objectFit="contain"
-         />
-       </div>
-       <div className={styles.bg2}>
-         <Image
-           alt="Mountains"
-           src={BackgroundShape}
-           layout="fill"
-           objectFit="contain"
-         />
-       </div>
-       
+    <div className='wrapper'>
+      <section className={styles.hero}>
+        <div className={styles.bg1}>
+          <Image
+            alt="Mountains"
+            src={BackgroundShape}
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <div className={styles.bg2}>
+          <Image
+            alt="Mountains"
+            src={BackgroundShape}
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
       
-       <div
-       className={getClasses({ classes: "container grid", styles, stylesClass: "hero-container" })}
-       >
-         <div className={styles["hero-content"]}>
-           <div className={styles["hero-social"]}>
-             <a href="" target="_blank" className={styles["hero-social-icon"]}>
-               <i class="fa-brands fa-linkedin-in"></i>
-             </a>
-             <a href="" target="_blank" className={styles["hero-social-icon"]}>
-               <i class="fa-brands fa-github"></i>
-             </a>
-             <a href="" target="_blank" className={styles["hero-social-icon"]}>
-               <i class="fa-brands fa-twitter"></i>
-             </a>
+        <div
+        className={getClasses({ classes: "container grid", styles, stylesClass: "hero-container" })}
+        >
+          <div className={styles["hero-content"]}>
+            <div className={styles["hero-social"]}>
+              <a href="" target="_blank" className={styles["hero-social-icon"]}>
+                <i class="fa-brands fa-linkedin-in"></i>
+              </a>
+              <a href="" target="_blank" className={styles["hero-social-icon"]}>
+                <i class="fa-brands fa-github"></i>
+              </a>
+              <a href="" target="_blank" className={styles["hero-social-icon"]}>
+                <i class="fa-brands fa-twitter"></i>
+              </a>
+            </div>
+      
+           <div className={styles["hero-img"]} >
+               <Image
+                 alt="Mountains"
+                 src={ProfilePhoto}
+                 layout="fill"
+                 objectFit="contain"
+               />
            </div>
+            <div className={styles["hero-data"]}>
+              <h1 className={styles["hero-title"]}>Hi, I am <span>Rulo</span> </h1>
+              <h3 className={styles["hero-subtitle"]}>Frontend developer</h3>
       
-          <div className={styles["hero-img"]} >
-              <Image
-                alt="Mountains"
-                src={ProfilePhoto}
-                layout="fill"
-                objectFit="contain"
-              />
+              <p className={styles["hero-description"]}>
+                Creating the next web generation. I want to make sure that the
+                internet is a safe, satisfying and secure place for everyone.
+              </p>
+              <a href="" className="button">Let’s Talk !</a>
+            </div>
+            {/* <div className="hero-scroll">
+              <a href="#about" className={getClasses({ classes: "button button--flex", styles, stylesClass: "hero-scroll-button" })}>
+             
+              <i class="uil uil-mouse-alt"></i>
+              <span>Scroll down</span>
+              <i class="uil uil-arrow-down"></i>
+              </a>
+            </div> */}
           </div>
-           <div className={styles["hero-data"]}>
-             <h1 className={styles["hero-title"]}>Hi, I am <span>Rulo</span> </h1>
-             <h3 className={styles["hero-subtitle"]}>Frontend developer</h3>
+        </div>
       
-             <p className={styles["hero-description"]}>
-               Creating the next web generation. I want to make sure that the
-               internet is a safe, satisfying and secure place for everyone.
-             </p>
-             <a href="" className="button">Let’s Talk !</a>
-           </div>
-           {/* <div className="hero-scroll">
-             <a href="#about" className={getClasses({ classes: "button button--flex", styles, stylesClass: "hero-scroll-button" })}>
-            
-             <i class="uil uil-mouse-alt"></i>
-             <span>Scroll down</span>
-             <i class="uil uil-arrow-down"></i>
-             </a>
-           </div> */}
-         </div>
-       </div>
-     </div>
-    
-      
-    </section>
+        
+      </section>
+    </div>
   );
 };
